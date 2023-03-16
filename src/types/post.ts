e4619d535d6fc97e3e784945d0f6bd55cb523703
Yusuke <yhakamaya@adobe.com>
2023-03-16 18:09:26 +0900

    add Post type
    for better parsing

---
+++ src/types/post.ts
@@ -0,0 +1,77 @@
+export type Post = {
+  id: number;
+  date: string;
+  date_gmt: string;
+  guid: {
+    rendered: string;
+  };
+  modified: string;
+  modified_gmt: string;
+  slug: string;
+  status: string;
+  type: string;
+  link: string;
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+  };
+  excerpt: {
+    rendered: string;
+  };
+  author: number;
+  featured_media: number;
+  comment_status: string;
+  ping_status: string;
+  sticky: boolean;
+  template: string;
+  format: string;
+  meta: [];
+  categories: number[];
+  tags: number[];
+  _links: {
+    self: {
+      href: string;
+    };
+    collection: {
+      href: string;
+    };
+    about: {
+      href: string;
+    };
+    author: {
+      embeddable: boolean;
+      href: string;
+    };
+    replies: {
+      embeddable: boolean;
+      href: string;
+    };
+    "version-history": {
+      count: number;
+      href: string;
+    };
+    "predecessor-version": {
+      id: number;
+      href: string;
+    };
+    "wp:featuredmedia": {
+      embeddable: boolean;
+      href: string;
+    };
+    "wp:attachment": {
+      href: string;
+    };
+    "wp:term": {
+      taxonomy: string;
+      embeddable: boolean;
+      href: string;
+    };
+    curies: {
+      name: string;
+      href: string;
+      templated: boolean;
+    };
+  };
+};
